631c7c529cff881be562011190aca23a2d5722bb
Maria Stachie <mary.stachie@gmail.com>
2022-03-14 16:54:14 +0100

    works but not done

--- js-pali1/script.js
+++ js-pali1/script.js
@@ -5,13 +5,13 @@
 
 // Chiedere all'utente una parola:
 
-const userWord = askUserWord('Inserisci una parola, sarà polindroma?');
+const userWord = prompt('Inserisci una parola, sarà polindroma?');
 
-
+/* Siccome chiedeva solo il prompt, non c'è bisogno di crearla
 function askUserWord(message) {
     const userInput = prompt(message);
     return userInput;
-}
+}*/
 
 // console.log(userWord);
 
@@ -20,9 +20,13 @@ function askUserWord(message) {
 // Creare una funzione per capire se la parola inserita è polindroma
 
 
-// creo una variabile che mi 
+// creo una variabile che mi divide la parola in unici caaratteri
 let inLettere = userWord.split('');
 console.log('La parola in singole lettere è: ' + inLettere);
+
+// senza farlo con split, ma manualmente:
+
+
 
 let alContrario = inLettere.reverse();
 console.log('La parola al contrario è: ' + alContrario);
@@ -30,14 +34,14 @@ let valid = false;
 
 function isParolaPalindroma(poli) {
     for (i = 0; i < inLettere.length; i++) {
-        if (poli == inLettere[i] || poli == alContrario[i]) {
+        if (inLettere[0] === poli[0]) {
             valid = true;
             return 'La parola è PALINDROMA!!!! ';
         }
-        if (poli !== inLettere) {
-            valid = false;
-            return 'la parola non è palindroma';
-        }
+    }
+    if (poli !== inLettere) {
+        valid = false;
+        return 'la parola non è palindroma';
     }
 }
 
